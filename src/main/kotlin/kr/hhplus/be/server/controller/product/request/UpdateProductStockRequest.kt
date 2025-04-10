45c25099170ad768786b002531efb4a270936fa4
DoHoon Ra <citron0137@gmail.com>
2025-04-11 06:52:17 +0900

    상품 관련 기능 구현: ProductFacade, ProductController, 요청 및 응답 DTO 추가, 상품 조회, 생성, 재고 업데이트, 증가 및 감소 기능 구현. 관련 단위 테스트 작성.

--- src/main/kotlin/kr/hhplus/be/server/controller/product/request/UpdateProductStockRequest.kt
+++
@@ -1,5 +0,0 @@
-package kr.hhplus.be.server.controller.product.request
-
-class UpdateProductStockRequest (
-    val stock: Long,
-)
--- src/main/kotlin/kr/hhplus/be/server/controller/product/response/ProductResponseDTO.kt
+++
@@ -1,8 +0,0 @@
-package kr.hhplus.be.server.controller.product.response
-
-class ProductResponseDTO(
-    val id: String,
-    val name: String,
-    val price: Long,
-    val stock: Long,
-)
