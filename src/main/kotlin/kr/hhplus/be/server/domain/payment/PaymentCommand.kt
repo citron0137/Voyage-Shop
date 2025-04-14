ac533839e9b149e0724e849e76dd083321c6c7d2
DoHoon Ra <citron0137@gmail.com>
2025-04-15 00:50:02 +0900

    refactor: Payment 도메인 Command, Query 클래스 분리

--- src/main/kotlin/kr/hhplus/be/server/domain/payment/PaymentCommand.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/payment/PaymentCommand.kt
@@ -1,10 +1,16 @@
 package kr.hhplus.be.server.domain.payment
 
-class PaymentCommand {
-    class Create(
+/**
+ * 결제 도메인 명령 관련 클래스
+ */
+sealed class PaymentCommand {
+    /**
+     * 결제 생성 명령
+     */
+    data class Create(
         val userId: String,
         val totalPaymentAmount: Long
-    ) {
+    ) : PaymentCommand() {
         init {
             if (userId.isBlank()) throw PaymentException.UserIdShouldNotBlank("사용자 ID는 비어있을 수 없습니다.")
             if (totalPaymentAmount <= 0) throw PaymentException.PaymentAmountShouldMoreThan0("결제 금액은 0보다 커야합니다.")
