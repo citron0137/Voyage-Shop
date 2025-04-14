84a6f50a44ea02b3241067ab8e8ec9b29b000ed0
DoHoon Ra <citron0137@gmail.com>
2025-04-14 23:11:07 +0900

    리팩토링: UserResponseSchema 클래스명을 UserSchema로 변경

--- src/main/kotlin/kr/hhplus/be/server/controller/user/UserSchema.kt
+++ src/main/kotlin/kr/hhplus/be/server/controller/user/UserSchema.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component
  * 사용자 도메인 관련 Swagger 스키마 정의
  */
 @Component
-class UserResponseSchema : SchemaProvider {
+class UserSchema : SchemaProvider {
 
     override fun getSchemas(): Map<String, Schema<Any>> {
         val schemas = mutableMapOf<String, Schema<Any>>()
