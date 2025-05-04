d9ae634182eda5e6b54dfee828da1ed5eaf1b7f1
DoHoon Ra <citron0137@gmail.com>
2025-05-04 12:13:32 +0900

    ✨ [FEATURE] 주문 관련 기능 추가
    - 주문 아이템 조회를 위한 `findByOrderIdIn` 메서드 추가
    - 최근 N일간의 상품별 주문 수량을 집계하는 `GetAggregatedOrderItems` 쿼리 추가
    - 주문 생성일 기준으로 주문을 조회하는 `findByCreatedAtAfter` 메서드 추가
    - 상품별 주문 수량 집계를 위한 `getAggregatedOrderItemsByProductId` 메서드 구현

--- src/main/kotlin/kr/hhplus/be/server/domain/order/OrderItemRepository.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/order/OrderItemRepository.kt
@@ -5,4 +5,5 @@ interface OrderItemRepository {
     fun createAll(orderItems: List<OrderItem>): List<OrderItem>
     fun findById(orderItemId: String): OrderItem?
     fun findByOrderId(orderId: String): List<OrderItem>
+    fun findByOrderIdIn(orderIds: List<String>): List<OrderItem>
 } 
